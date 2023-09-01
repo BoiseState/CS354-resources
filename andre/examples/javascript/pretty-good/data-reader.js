4963bae91e9390443c29d7a027298526f0493696
andrekeys <andrekeys@boisestate.edu>
2023-09-01 13:44:55 -0600

    adding accessor examples to data-reader.js

--- andre/examples/javascript/pretty-good/data-reader.js
+++ andre/examples/javascript/pretty-good/data-reader.js
@@ -2,3 +2,10 @@
 const data = require("./data.json");
 
 console.log(JSON.stringify(data));
+
+//can also access specific fields to get their values, like a hashtable! 
+console.log(data.key);
+
+//or access it with a variable, in case you don't know what the field is
+let string = 'key2';
+console.log(data[string]);
