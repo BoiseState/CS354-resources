d23858186679087172d92a6a66e3c46be37f99eb
andrekeys <andrekeys@boisestate.edu>
2023-09-01 15:58:24 -0600

    adding p2 files

---
+++ andre/projects/project2/java/Parser.java
@@ -0,0 +1,76 @@
+import java.io.File;
+
+public class Parser {
+
+    private Store store;
+
+    /**
+     *
+     * @param filename - a file with data that is assumed to be valid JSON
+     * @param store -
+     */
+    public Parser(String filename, Store store) {
+        File file = new File(filename);
+        this.store = store;
+    }
+
+    /**
+     * The parse method recursively traverses the JSON found in file,
+     * and if it finds a specific tag,
+     *
+     * For example, if the input JSON is
+     * {
+     * "type": "FeatureCollection",
+     * "features": [
+     * {
+     *   "type": "Feature",
+     *   "id": "relation/6023246",
+     *   "properties": {
+     *     "timestamp": "2022-09-20T01:21:58Z",
+     *     "version": "14",
+     *     "changeset": "126400845",
+     *     "user": "bhietsch",
+     *     "uid": "5390272",
+     *     "boundary": "protected_area",
+     *     "name": "Boise National Forest",
+     *     "operator": "United States Forest Service",
+     *     "operator:short": "USFS",
+     *     "operator:type": "public",
+     *     "operator:wikidata": "Q1891156",
+     *     "operator:wikipedia": "en:United States Forest Service",
+     *     "ownership": "national",
+     *     "protect_class": "6",
+     *     "protected": "perpetuity",
+     *     "protection_title": "National Forest",
+     *     "type": "boundary",
+     *     "id": "relation/6023246"
+     *   }
+     *  },
+     *  {
+     *   "type": "Feature",
+     *   "id": "relation/15486139",
+     *   "properties": {
+     *     "timestamp": "2023-02-16T22:24:11Z",
+     *     "version": "1",
+     *     "changeset": "132650868",
+     *     "user": "ottwiz",
+     *     "uid": "3087157",
+     *     "natural": "wood",
+     *     "type": "multipolygon",
+     *     "id": "relation/15486139"
+     *   }
+     *  ]
+     * }
+     *
+     *     and tag is "user"
+     *     Parser will add to the store
+     *     "bhietsch"
+     *     "ottwiz"
+     *
+     */
+    public void parse(String tag) {
+        /*
+            TODO:
+         */
+    }
+}
---
+++ andre/projects/project2/java/Store.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class Store {
+
+    private HashMap<String, Integer> map;
+
+    public Store() {
+        map = new HashMap<>();
+    }
+
+    /**
+     * Insert a key into the Store.
+     * If the key is already in the store, its value is incremented,
+     * indicating the frequency that the key has been added to the store.
+     *
+     * @param key - the key to insert
+     *
+     *     If called in this succession:
+     *     "bhietsch"
+     *     "ottwiz"
+     *     "ottwiz"
+     *
+     *     The map will contain:
+     *     "bhietsch" 1
+     *     "ottwiz"   2
+     *
+     */
+    public void addToMap(String key) {
+
+        if (map.containsKey(key)) {
+            Integer num = map.get(key);
+            map.replace(key, num + 1);
+        } else {
+            map.put(key, 1);
+        }
+    }
+
+    /**
+     * Transform the Store into a
+     * @return a String the contains each value, in the following format
+     *
+     *
+     *     If the map contains:
+     *     "bhietsch" 1
+     *     "ottwiz"   2
+     *
+     *     The string produced will be:
+     *     "bhietsch: 1
+     *     ottwiz: 2
+     *     "
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Set<String> keys = map.keySet();
+
+        for (String key : keys) {
+            Integer value = map.get(key);
+            sb.append(key).append(": ").append(value).append("\n");
+        }
+        return sb.toString();
+    }
+}
