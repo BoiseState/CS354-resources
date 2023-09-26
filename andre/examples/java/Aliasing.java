b5c9c5aa4a62a005aaa4b82450e1e0301729c646
andrekeys <andrekeys@boisestate.edu>
2023-09-26 12:12:57 -0600

    adding example

---
+++ andre/examples/java/Aliasing.java
@@ -0,0 +1,26 @@
+public class Aliasing {
+
+    private int a;
+
+    public Aliasing(int a) {
+        this.a = a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return this.a;
+    }
+
+    public static void main(String[] args) {
+        Aliasing obj = new Aliasing(1);
+        Aliasing alias = obj;
+
+        alias.setA(6);
+
+        System.out.println(obj.getA());
+    }
+    
+}
