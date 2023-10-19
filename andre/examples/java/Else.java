e43b0cb4126772bf504c4c21e62e27bf553b530f
andrekeys <andrekeys@boisestate.edu>
2023-10-19 11:34:08 -0600

    another example

---
+++ andre/examples/java/Else.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Else
+{
+    public static void main(String args[])
+    {
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter num: ");
+        int n=sc.nextInt();
+        if(n>0)
+        {
+            if(n>100)
+                System.out.println("more than 100.");
+        }
+        else
+            System.out.println("The given number is negative.");
+    }
+}
