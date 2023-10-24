43426401e884d7f293238259292ec729f21b9b7d
andrekeys <andrekeys@boisestate.edu>
2023-10-24 11:47:33 -0600

    adding example

---
+++ andre/examples/java/MyIterator.java
@@ -0,0 +1,25 @@
+
+import java.util.Iterator;
+import java.util.ArrayList;
+
+
+public class MyIterator {
+
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> collection = new ArrayList<>();
+
+		for (int i = 0; i < 10; i++ ) {
+			collection.add(i);
+		}
+
+		Iterator<Integer> it = collection.iterator(); 
+
+		while (it.hasNext()) {
+
+			System.out.println(it.next());
+		}
+	}
+
+}
