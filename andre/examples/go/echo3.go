d1a785142d71805e60fc35e5f4099121d1abc9d0
andrekeys <andrekeys@boisestate.edu>
2023-11-02 09:53:10 -0600

    adding go examples

---
+++ andre/examples/go/echo3.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	
+
+
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
---
+++ andre/examples/go/loops.go
@@ -0,0 +1,14 @@
+//go has only for loops
+
+package main
+
+//traditional 'while' 
+for condition {
+
+}
+
+//traditional infinite loop
+
+for {
+
+}
