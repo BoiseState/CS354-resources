8cc0ef8915bdb372ab1ba91dde595f4df5d54dad
andrekeys <andrekeys@boisestate.edu>
2023-12-16 09:20:04 -0700

    improving structure of java project

---
+++ andre/projects/p1/java/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Depositing x money into Checking Account #1234");
+
+		//then demonstrate race condition
+
+
+	}
+
+
+}
---
+++ andre/projects/p1/java/bank/Bank.java
@@ -0,0 +1,28 @@
+package bank;
+
+import java.util.*;
+
+public class Bank {
+
+    private Set<Account> accounts=new HashSet<Account>();
+
+    public void add(Account account) {
+		accounts.add(account);
+    }
+
+    public void accrue(double rate) {
+		for (Account account: accounts) {
+			account.accrue(rate);
+		}
+    }
+
+    public String toString() {
+		String string = "";
+		for (Account account: accounts) {
+			string += account + "\n";
+		}
+		return string ;
+    }
+
+
+}
