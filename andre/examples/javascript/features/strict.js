5d8cc035a7226f194add2ed3ef7f5da53e8864ae
andrekeys <andrekeys@boisestate.edu>
2024-03-24 21:51:16 -0600

    adding strict example

--- andre/examples/javascript/features/strict.js
+++ andre/examples/javascript/features/strict.js
@@ -1,6 +1,27 @@
 //"use strict";
 variable = 5;
-
 console.log(variable);
 
 //try it again with the strict comment above!
+
+//Strict scope ??? Why would anyone agree to this.
+
+function sloppy() {
+    variable = 5
+}
+
+(function() {
+    "use strict";
+    sloppy()
+})()
+
+function strict() {
+    "use strict";
+    variable = 5
+}
+
+(function() {
+    strict()
+})()
+
+//credit: https://dev.to/frehner/exploring-javascript-s-strict-mode-8ip
