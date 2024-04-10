1146651cae440dcbdfe47310b0f42431957c8919
andrekeys <andrekeys@boisestate.edu>
2024-04-10 09:24:20 -0600

    go version

---
+++ andre/examples/go/module6/sqrt.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func sqrt(x float64) float64 {
+	average := func(x float64, y float64) float64 {
+		return (x + y) / 2
+	}
+	abs := func(x float64) float64 {
+		if x < 0 {
+			return -x
+
+		} else {
+			return x
+		}
+	}
+	square := func(x float64) float64 {
+		return x * x
+	}
+	goodEnough := func(guess float64) bool {
+		return abs(square(guess)-x) < 0.001
+	}
+	improve := func(guess float64) float64 {
+		return average(guess, (x / guess))
+	}
+	sqrtIter := func(guess float64) float64 {
+		if goodEnough(guess) {
+			return guess
+		} else {
+			return sqrtIter(improve(guess)) //stuck!
+		}
+	}
+	return sqrtIter(1.0)
+}
+
+func main() {
+	fmt.Println(sqrt(9))
+}
