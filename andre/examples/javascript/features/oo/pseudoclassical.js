8e791bb698b67f02f8218eb6bf5e619b93e9da4c
andrekeys <andrekeys@boisestate.edu>
2024-09-25 15:27:13 -0600

    updating example

--- andre/examples/javascript/features/oo/pseudoclassical.js
+++ andre/examples/javascript/features/oo/pseudoclassical.js
@@ -1,3 +1,32 @@
+// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
+
+const parent = {
+    value: 2,
+    method() {
+        return this.value + 1;
+    },
+};
+
+console.log(parent.method()); // 3
+// When calling parent.method in this case, 'this' refers to parent
+
+// child is an object that inherits from parent
+const child = {
+    __proto__: parent,
+};
+console.log(child.method()); // 3
+// When child.method is called, 'this' refers to child.
+// So when child inherits the method of parent,
+// The property 'value' is sought on child. However, since child
+// doesn't have an own property called 'value', the property is
+// found on the [[Prototype]], which is parent.value.
+
+child.value = 4; // assign the value 4 to the property 'value' on child.
+// This shadows the 'value' property on parent.
+// The child object now looks like:
+// { value: 4, __proto__: { value: 2, method: [Function] } }
+console.log(child.method()); // 5
+
 // JavaScript was worried about using too unfamiliar of concepts for mainstream programmers at the time
 // Keywords and concepts that you are used to appear, but they do different things.
 // example: 'new' operator and constructor functions
