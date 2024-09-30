7bbd34b4bf965502dffe21187800bdaea01aa87e
andrekeys <andrekeys@boisestate.edu>
2024-09-30 14:43:06 -0600

    updating example

--- andre/examples/javascript/features/eslint/app.js
+++ andre/examples/javascript/features/eslint/app.js
@@ -1,1 +1,32 @@
-var test = 4
+var test = 4 // var is bad
+
+// hoisted function
+function myFunc() {
+    console.log("hello!") // ugly double-quoted string
+    console.log(`hello!`) // single quoted string without interpolation
+
+    // ugly formatting
+}
+
+if (0 == '0') {
+    console.log("yuck!")
+}
+
+const item = new Object(); // use object literal instead
+
+function handlgeThings(opts) {
+    opts = opts || {};
+}
+
+// default paramters
+var b = 1;
+function count( a = b++) {
+    console.log(a)
+}
+
+// too many spaces
+
+
+
+
+
