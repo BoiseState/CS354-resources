48864cc3bea9b713e02bc0b0119c0c52402e88c3
andrekeys <andrekeys@boisestate.edu>
2024-10-07 16:20:20 -0600

    adding example

---
+++ andre/examples/javascript/module3/higher-order.js
@@ -0,0 +1,15 @@
+
+
+const f1 = function (a) {
+    return function () { console.log(a); }
+}
+
+let variable = 2;
+
+const f2 = f1(variable); // f2 is the name of a function that was returned from f1.
+
+variable += 1; // variable is now 3
+
+f2(); //what gets printed?
+
+console.log(variable);
