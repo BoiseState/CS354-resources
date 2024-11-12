7bae4101e9b2e5a4d3bc3c9075fcb0f7260cbc09
andrekeys <andrekeys@boisestate.edu>
2024-11-11 20:34:39 -0700

    updating

---
+++ andre/examples/scheme/module6/myif.scm
@@ -0,0 +1,30 @@
+(define (myif predicate then-clause else-clause)
+  (cond (predicate then-clause)
+    (else else-clause)))
+
+(display (myif (= 2 3) 0 5)) (newline)
+(display (myif (= 1 1) 0 5)) (newline)
+
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (display (good-enough? guess x))
+  (myif (good-enough? guess x) ;modified
+    guess
+    (sqrt-iter (improve guess x)
+      x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(display (sqrt 9))
