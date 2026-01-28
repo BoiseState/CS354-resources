6199bcc0375831e53372eb6dfa9ef9574a60963c
andrekeys <andrekeys@boisestate.edu>
2026-01-28 09:45:28 -0700

    updating tree printing for ia2

--- andre/projects/ia/ia2/syntax/ParserTest.java
+++ andre/projects/ia/ia2/syntax/ParserTest.java
@@ -14,7 +14,7 @@ class ParserTest {
         Parser parser = new Parser();
         String prg = "x";
 
-        System.out.println(formatString(parser.parse(prg).toString()));
+        System.out.println(parser.parse(prg).toString());
     }
 
 
@@ -23,7 +23,7 @@ class ParserTest {
         Parser parser = new Parser();
         String prg = "x + 3";
 
-        System.out.println(formatString(parser.parse(prg).toString()));
+        System.out.println(parser.parse(prg).toString());
     }
 
     @Test
@@ -36,40 +36,4 @@ class ParserTest {
             parser.parse(prg);
         });
     }
-
-    /**
-     * Helper method that simply adds newlines and tabs to a string where there are ( and )
-     * @param tree - Non formatted version of the tree
-     * @return formatted version of the String
-     */
-    private String formatString(String tree) {
-        StringBuilder sb = new StringBuilder();
-
-        int indents = 0;
-        int position = 0;
-        int old = 0;
-
-        while (position < tree.length()) {
-            if (tree.charAt(position) == '(') {
-                sb.append(tree, old, position + 1);
-                old = position + 1;
-                indents++;
-                sb.append('\n');
-                sb.append("\t".repeat(indents));
-            }
-            if (tree.charAt(position) == ')') {
-                sb.append(tree, old, position);
-                old = position + 1;
-                sb.append('\n');
-                indents--;
-                sb.append("\t".repeat(indents));
-                sb.append(" )\n");
-                sb.append("\t".repeat(indents));
-            }
-            position++;
-        }
-
-        return sb.toString();
-    }
-
 }
