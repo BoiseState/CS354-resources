036d2308c051766b9d90c2f6ff695cbbc730c0e5
andrekeys <andrekeys@boisestate.edu>
2026-02-26 09:09:59 -0700

    more closure

--- andre/examples/javascript/module6/fib.js
+++ andre/examples/javascript/module6/fib.js
@@ -2,3 +2,17 @@ const fibonacci = function (n) {
     if (n < 2) return n;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+// count how many times fib is called by hiding the count in a closure
+const fibCount = function() {
+    let count = 0;
+
+    return {
+        fib: function(n) {
+            count += 1;
+            if (n < 2) return n;
+            return this.fib(n - 1) + this.fib(n - 2);
+        },
+        getCount: function() { return count;}
+    }
+}
